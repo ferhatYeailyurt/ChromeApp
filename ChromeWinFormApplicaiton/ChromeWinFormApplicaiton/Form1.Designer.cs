9ab1d53381258e7e9945ca9d4de7d444d5dd74ba
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2018-07-28 13:04:13 +0300

    CefSSharp included and form element created.

--- ChromeWinFormApplicaiton/ChromeWinFormApplicaiton/Form1.Designer.cs
+++ ChromeWinFormApplicaiton/ChromeWinFormApplicaiton/Form1.Designer.cs
@@ -28,21 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnReflesh = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.pContainer = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(13, 13);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(44, 24);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(63, 13);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(44, 24);
+            this.btnForward.TabIndex = 0;
+            this.btnForward.Text = ">";
+            this.btnForward.UseVisualStyleBackColor = true;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(194, 16);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(566, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnReflesh
+            // 
+            this.btnReflesh.Location = new System.Drawing.Point(113, 14);
+            this.btnReflesh.Name = "btnReflesh";
+            this.btnReflesh.Size = new System.Drawing.Size(75, 23);
+            this.btnReflesh.TabIndex = 2;
+            this.btnReflesh.Text = "Reflesh";
+            this.btnReflesh.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(767, 13);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(54, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            // 
+            // pContainer
+            // 
+            this.pContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pContainer.Location = new System.Drawing.Point(12, 43);
+            this.pContainer.Name = "pContainer";
+            this.pContainer.Size = new System.Drawing.Size(809, 456);
+            this.pContainer.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 414);
+            this.ClientSize = new System.Drawing.Size(833, 511);
+            this.Controls.Add(this.pContainer);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnReflesh);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chrome Form";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnReflesh;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Panel pContainer;
     }
 }
 
